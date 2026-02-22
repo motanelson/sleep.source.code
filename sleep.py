b7a898bd0f746bcaa2bf51b4a8765f8775958779
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-22 12:36:31 +0000

    Add files via upload

--- sleep.py
+++ sleep.py
@@ -1,17 +1,5 @@
-import time
-import os
-import datetime
-def sleep(s:int):
-    x = datetime.datetime.now()
-    while 1:
-        xx = datetime.datetime.now()
-    
-        xx=xx-x
-        xx=xx.seconds
-        if xx>=s:
-            break
-
+import times.times
 print("\033c\033[40;37m\n please wait")
-sleep(5)
+times.times.sleep(5)
 
 
