5fa6a3db16b6b4644caa2c973291a523673512b7
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-03-06 08:37:39 +0000

    Add files via upload

---
+++ value.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from scipy import stats
+import copy
+
+# Dados
+
+def calcX(i:int):
+   ii=[]
+   for n in range(i):
+       nn=[]
+       nn.append(copy.copy(n))
+       xx=float(n)*2.25
+       nn.append(copy.copy(xx))
+       ii.append(copy.copy(nn))
+   return ii
+def calcY(X):
+    ii=[]
+    for n in X:
+        xx=n[0]+n[1]
+        ii.append(copy.copy(xx))
+    return ii
+def prints(tt):
+    for t in tt:
+      print(int(t),end=" , ")    
+X=calcX(20)
+XX=calcX(40)
+y=calcY(X)
+yy=calcY(XX)
+print(yy)
+print("-"*40)
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+tt=[]
+for n in range(40):
+    tt=regr.predict(XX)
+    
+    
+    
+prints(tt)
